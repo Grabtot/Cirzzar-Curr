c22164eceacd91a20acf6e26cc00a68f6b98b2ec
Grabtot <dneshotkin@gmail.com>
2023-04-12 13:49:51 +0300

    Product, Pizza and Ingredients classes added

---
+++ Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using CirzzarCurr.Models.Enums;
+using System.Drawing;
+
+namespace CirzzarCurr.Models
+{
+    public abstract class Product
+    {
+        public virtual decimal Price { get; set; }
+        public string Name { get; set; }
+        public int? Size { get; set; }
+        public Image Image { get; set; }
+
+        public ProductType Type { get; set; }
+        public Product()
+        {
+            Name = string.Empty;
+            Size = 0;
+        }
+        protected Product(decimal price, string name, int? size, Image image, ProductType type)
+        {
+            Price = price;
+            Name = name;
+            Size = size;
+            Image = image;
+            Type = type;
+        }
+        protected Product(ProductType type) : this()
+        {
+            Type = type;
+        }
+    }
+}
