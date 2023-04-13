2a0590ed22758ee127a9fc6cfa0a3f004ad5db17
Grabtot <dneshotkin@gmail.com>
2023-04-13 16:48:27 +0300

    Add product repository #11

---
+++ Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using CirzzarCurr.Models;
+using CirzzarCurr.Models.Enums;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CirzzarCurr.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<IEnumerable<TProduct>> GetByTypeAsync<TProduct>(ProductType type) where TProduct : Product;
+        Task<Product> GetByIdAsync(int id);
+        Task<Product> AddAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task DeleteByIdAsync(int id);
+    }
+}
---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using CirzzarCurr.Data;
+using CirzzarCurr.Models;
+using CirzzarCurr.Models.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace CirzzarCurr.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> AddAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            var product = await GetByIdAsync(id);
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id)
+                ?? throw new IndexOutOfRangeException("Wrong index");
+        }
+
+        public async Task<IEnumerable<TProduct>> GetByTypeAsync<TProduct>(ProductType type) where TProduct : Product
+        {
+            return (await _context.Products.Where(prod => prod.Type == type).ToListAsync()).OfType<TProduct>();
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            product = _context.Products.Update(product).Entity;
+            await _context.SaveChangesAsync();
+            return product;
+        }
+    }
+}
